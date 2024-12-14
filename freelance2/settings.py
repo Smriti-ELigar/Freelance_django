1d88152dcdd04df9ecf3159ba1fb6751850a5c6e
Smriti-ELigar <smritieligar1005@gmail.com>
2024-12-14 18:05:12 +0530

    verified and made changes for  authentication

--- freelance2/settings.py
+++ freelance2/settings.py
@@ -128,19 +128,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Configure the default site in settings.py (for email verification)
 SITE_ID = 1 
 AUTH_USER_MODEL = 'users.CustomUser'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-EMAIL_HOST_USER = 'your-email@example.com'
-EMAIL_HOST_PASSWORD = 'your-email-password'
